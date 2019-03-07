352e05a36ccbdbe96dc6dc3c2e3edb6998e3ed73
Kartik Kishore <kartik@Kartiks-MacBook-Pro.local>
2019-03-07 15:36:24 +1100

    ATC Working model

--- Scraper1.py
+++ Scraper1.py
@@ -27,21 +27,38 @@ def atc():
     alphabet = 'A'
     searchBox.send_keys(alphabet)
     driver.find_element_by_class_name('button').click()
+
+    # First level Scraping
     text = driver.find_element_by_id('content').text.split('\n')
     del text[0:2]
     del text[-1]
     firstCodes = [item[0:3] for item in text]
-    driver.get('https://www.whocc.no/atc_ddd_index/' + '?code=' + firstCodes[8])
+    print('First Level: ', firstCodes, '\n')
+    driver.get('https://www.whocc.no/atc_ddd_index/' + '?code=' + firstCodes[15])
 
-    #######
+    # Second level Scraping
     text2 = driver.find_element_by_id('content').text.split('\n')
     del text2[0:2]
     del text2[-1]
     del text2[0]
     secondCodes = [item.split(' ')[0] for item in text2]
-    print(secondCodes)
+    print('Second Level: ', secondCodes, '\n')
     driver.get('https://www.whocc.no/atc_ddd_index/' + '?code=' + secondCodes[0])
-    # print(driver.find_element_by_id('content').text)
+
+    # Third level Scraping
+    text3 = driver.find_element_by_id('content').text.split('\n')
+    del text3[0:3]
+    del text3[-1]
+    del text3[0]
+    thirdCodes = [item.split(' ')[0] for item in text3]
+    print('Third Level: ', thirdCodes, '\n')
+    driver.get('https://www.whocc.no/atc_ddd_index/' + '?code=' + thirdCodes[0])
+
+    # Fourth level Scraping
+    text4 = driver.find_element_by_xpath('//*[@id="content"]/ul/table/tbody').text.split('\n')
+    del text4[0]
+    text4 = [item.strip() for item in text4]
+    print('Fourth level: ', text4)
 
 
 if __name__ == '__main__':
